0b6c3acd35a38f946a758e146bed2ad8509ea36c
John Anthony Bataller <johnanthonybataller.ce@gmail.com>
2021-08-27 01:05:03 +0800

    updated all pages, added cart page

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,33 @@
-addToCartButton = document.querySelectorAll(".add-to-cart-button");
+let subscribeBtn = document.querySelector("#subscribeBtn")
+
+subscribeBtn.addEventListener("click", function(e){
+  e.preventDefault()
+
+  Swal.fire({
+    title: 'Success!',
+    text: 'Thank you for subscribing to our newsletter.',
+    icon: 'success',
+    confirmButtonText: 'Close'
+  })
+})
+
+
+let addToCartButton = document.querySelectorAll(".add-to-cart-button");
 
 document
   .querySelectorAll(".add-to-cart-button")
   .forEach(function (addToCartButton) {
     addToCartButton.addEventListener("click", function () {
+      
+      // INCREMENT CART VALUE
+      var value = parseInt(document.getElementById('cartCount').value, 10);
+      value = isNaN(value) ? 0 : value;
+      value++;
+      document.getElementById('cartCount').value = value;
+      document.getElementById('cartCount').innerHTML = "";
+      document.getElementById('cartCount').innerHTML = ` <i class="fas fa-shopping-cart"></i> ${value}`;
+
       addToCartButton.classList.add("added");
-      iziToast.error({
-        title: data.errors.username,
-        message: "Unauthorized Access",
-        position: "topCenter",
-        timeout: 3000,
-    
-     });
     });
   });
 
@@ -40,3 +56,8 @@ document
       ]
     });
   });
+
+
+
+
+
